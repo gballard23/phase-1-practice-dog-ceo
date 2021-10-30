67754bc8e4b30861bd479503c8e2b86718d2e5bd
Gerren <g.ballard23@outlook.com>
2021-10-29 20:51:57 -0500

    commit tree 2011cc0192e4a550d24fc62f7d8ce8e37d41fe29

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 console.log('%c HI', 'color: firebrick')
+const imgURl = "https://dog.ceo/api/breeds/image/random/4" 
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+let allBreeds = []
+
+fetchImg()
+fetchDogBreeds()
+
+
+function fetchImg() {
+    fetch(imgURl)
+    .then(res => res.json())
+    .then(data => data.message.forEach(addImg))
+}
+
+function addImg(doge) {
+    const newImg = document.createElement('img')
+    newImg.src = doge
+    document.querySelector('#dog-image-container').appendChild(newImg)
+}
+
+function fetchDogBreeds() {
+    fetch(breedUrl)
+    .then(res => res.json())
+    .then(data => Object.keys(data.message).forEach((breed) => {
+        allBreeds = Object.keys(data.message)
+        const newLi = document.createElement('li')
+        newLi.innerText = breed
+        document.querySelector('#dog-breeds').appendChild(newLi)
+        newLi.addEventListener('click', (event) => {
+            const colors = ['orange', 'red', 'blue', 'purple', 'green']
+            const randomColor = colors[Math.floor(Math.random()*colors.length)]
+            event.target.style.color = randomColor
+        })
+    }))
+}
